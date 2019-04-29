196b4c3b1e0d58e13d799130f677b57a9de2ba6b
AidanMJW <Aidan_Walker@live.com.au>
2019-04-29 20:14:05 +1000

    Added Dust
    Enemys drop dust when killed. Player can collect dust.
    
    also added death effect to enemy.

--- Lucid, The Dream Wizard/Assets/Enemy.cs
+++ Lucid, The Dream Wizard/Assets/Enemy.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    float health = 100;
+    public float health = 100;
+    public Vector2Int lootDropRange = new Vector2Int();
+    public List<GameObject> loot = new List<GameObject>();
+    public GameObject deathEffect;
 
     void Update()
     {
@@ -23,6 +26,25 @@ public class Enemy : MonoBehaviour
 
     void die()
     {
+        dropLoot();
+        GameObject d = Instantiate(deathEffect);
+        d.transform.position = transform.position;
         Destroy(transform.gameObject);
+    }
+
+    void dropLoot()
+    {
+        int amount = Random.Range(lootDropRange.x, lootDropRange.y + 1);
+
+        if(amount != 0)
+        {
+            for(int i = 0; i < amount; i++)
+            {
+                GameObject l = Instantiate(loot[Random.Range(0, loot.Count)]);
+                Vector3 spawnPos = transform.position;
+                spawnPos.x += Random.Range(-0.1f, 0.1f);
+                l.transform.position = spawnPos;
+            }
+        }
     }
 }
---
+++ Lucid, The Dream Wizard/Assets/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float health = 100f;
+    [SerializeField]
+    int dust =0;
+
+    
+
+
+    public void addDust(int amount = 1)
+    {
+        dust = dust + amount;
+    }
+
+
+}
